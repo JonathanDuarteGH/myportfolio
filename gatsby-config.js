421324ca5ece9eaf7e4793f5bbebd275bb8f23fd
elgnis3900 <jonathanduarte313@gmail.com>
2020-12-01 09:38:54 -0500

    reconfigured gastby config due to fatal error

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,11 +29,11 @@ module.exports = {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: `http://localhost:1337`,
-        queryLimit: 1000, // Default to 100
-        //   contentTypes : `jobs`, `projects`, `blogs`,
-        //   singleType : `about`
-        //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
-        contentTypes: [],
+        queryLimit: 1000,
+        /*         //   contentTypes : `jobs`, `projects`, `blogs`,
+                //   singleType : `about`
+                //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!! */
+        contentTypes: [`jobs`],
         singleTypes: [],
       },
     },
--- src/constants/services.js
+++ src/constants/services.js
@@ -4,19 +4,19 @@ export default [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
-    title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "React/Gatsby Development",
+    text: `The easy-to-use, low cost deployment, and flexible development platform is widely used on all websites. Gatsby as a React web dev framework provides a static content management system (CMS), that allows you to easily make changes on the fly..`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
-    title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "responsive web design",
+    text: `Responsive web design allows your website to adapt to and provide an optimal experience on any device it’s being viewed from. This means that your website will look great and function flawlessly for a user on a desktop computer, laptop, tablet, or smartphone.`,
   },
   {
     id: 3,
     icon: <FaAndroid className="service-icon" />,
-    title: "app design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "website hosting",
+    text: `Our managed hosting services offer a flexible, low-cost web hosting solution tailored for your business. As your provider, we’ll take care of all daily hosting operations: software updates, server management, email hosting, and support—our team has you covered.`,
   },
 ]

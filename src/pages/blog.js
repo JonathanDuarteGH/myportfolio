30c5cf8aca4971c0c5baf2f7cac3f83b07591167
elgnis3900 <jonathanduarte313@gmail.com>
2020-12-08 21:32:08 -0500

    added side bar and footer functionality

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,10 +2,41 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
-// ...GatsbyImageSharpFluid
 
-const Blog = () => {
-  return <h2>blog page</h2>
+const Blog = ({ data }) => {
+  const {
+    allStrapiBlogs: { nodes: blogs },
+  } = data
+  return (
+    <Layout>
+      <section className="blog-page">
+        <Blogs blogs={blogs} title="all blogs" />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiBlogs {
+      nodes {
+        slug
+        content
+        description
+        date(formatString: "MMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Blog
